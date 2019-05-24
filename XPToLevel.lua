eefd253b9499cf1c6badd023283b6045f09d0e71
Eric Schlichting <ericschlichting@gmail.com>
2019-05-23 20:41:27 -0500

    First commit

---
+++ XPToLevel.lua
@@ -0,0 +1,28 @@
+---
+--- Created by Eric.
+--- DateTime: 5/22/2019 5:27 PM
+---
+
+local xpEventFrame = CreateFrame("Frame")
+
+xpEventFrame:RegisterEvent("PLAYER_XP_UPDATE")
+
+xpEventFrame:SetScript("OnEvent",
+    function(self, event, ...)
+        local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
+        local playerXp = UnitXP(arg1)
+        local maxXp = UnitXPMax(arg1)
+        local xpToLevel = maxXp - playerXp
+        print(comma_value(xpToLevel)..'xp left')
+    end)
+
+function comma_value(amount)
+        local formatted = amount
+        while true do
+                formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+                if (k==0) then
+                        break
+                end
+        end
+        return formatted
+end
