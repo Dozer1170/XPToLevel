97d3d13196e88df57c010c431d3435c6f0487b28
Eric Schlichting <ericschlichting@gmail.com>
2019-05-23 21:22:16 -0500

    Dont provide duplicate messages

--- XPToLevel.lua
+++ XPToLevel.lua
@@ -7,22 +7,26 @@ local xpEventFrame = CreateFrame("Frame")
 
 xpEventFrame:RegisterEvent("PLAYER_XP_UPDATE")
 
-xpEventFrame:SetScript("OnEvent",
-    function(self, event, ...)
-        local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
-        local playerXp = UnitXP(arg1)
-        local maxXp = UnitXPMax(arg1)
-        local xpToLevel = maxXp - playerXp
+local lastPlayerXp = 0
+
+xpEventFrame:SetScript("OnEvent", function(self, event, ...)
+    local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
+    local playerXp = UnitXP(arg1)
+    local maxXp = UnitXPMax(arg1)
+    local xpToLevel = maxXp - playerXp
+    if playerXp ~= lastPlayerXp then
         print(comma_value(xpToLevel)..'xp left')
-    end)
+    end
+    lastPlayerXp = playerXp
+end)
 
 function comma_value(amount)
-        local formatted = amount
-        while true do
-                formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
-                if (k==0) then
-                        break
-                end
-        end
-        return formatted
+    local formatted = amount
+    while true do
+            formatted, k = string.gsub(formatted, "^(-?%d+)(%d%d%d)", '%1,%2')
+            if (k==0) then
+                    break
+            end
+    end
+    return formatted
 end
