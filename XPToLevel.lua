6f594769355ba5474189f8509182a8af5c55c6bc
Eric Schlichting <ericschlichting@gmail.com>
2020-05-31 22:20:25 -0500

    Improvements

--- XPToLevel.lua
+++ XPToLevel.lua
@@ -1,24 +1,44 @@
 ---
---- Created by Eric.
+--- Created by Dozer1170.
 --- DateTime: 5/22/2019 5:27 PM
 ---
 
 local xpEventFrame = CreateFrame("Frame")
 
 xpEventFrame:RegisterEvent("PLAYER_XP_UPDATE")
+xpEventFrame:RegisterEvent("ADDON_LOADED")
 
 local lastPlayerXp = 0
+local killCount = 0
+local estKillsToGo = 0
 
 xpEventFrame:SetScript("OnEvent", function(self, event, ...)
     local arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9 = ...
-    local playerXp = UnitXP(arg1)
-    local maxXp = UnitXPMax(arg1)
-    local xpToLevel = maxXp - playerXp
-    if playerXp ~= lastPlayerXp then
-        print(comma_value(xpToLevel)..' xp left')
+
+    if (event == "ADDON_LOADED" and arg1 == "XPToLevel") then
+        lastPlayerXp = UnitXP("player")
+        print('XpToLevel loaded'..comma_value(lastPlayerXp))
     end
-    lastPlayerXp = playerXp
+
+    if (event == "PLAYER_XP_UPDATE") then
+        local playerXp = UnitXP("player")
+        local levelXp = UnitXPMax("player")
+        local xpToLevel = levelXp - playerXp
+        killCount = killCount + 1
+        local xpGained = playerXp - lastPlayerXp
+        estKillsToGo = math.floor(xpToLevel / xpGained)
+        
+        if playerXp ~= lastPlayerXp then
+            printXpToLevel(xpToLevel)
+            --print('playerXp:'..playerXp..' xpGained:'..xpGained)
+        end
+        lastPlayerXp = playerXp
+    end
 end)
+
+function printXpToLevel(xpToLevel)
+    print(comma_value(xpToLevel)..' xp left. ~'..estKillsToGo..' kills to level. Killount: '..comma_value(killCount))
+end
 
 function comma_value(amount)
     local formatted = amount
@@ -30,3 +50,11 @@ function comma_value(amount)
     end
     return formatted
 end
+
+SLASH_XPTOLEVEL1 = "/xptolevel"
+SlashCmdList["XPTOLEVEL"] = function(msg)
+    local playerXp = UnitXP("player")
+    local levelXp = UnitXPMax("player")
+    printXpToLevel(levelXp - playerXp)
+end
+
